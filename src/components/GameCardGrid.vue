20bd1b9fd79ce5ce116dd33b158c109010f0c91f
Dennis Burger <dennis.burger@poort80.nl>
2020-02-19 16:12:05 +0100

    Fixed grid view to spread it out over the width of the viewport

--- src/components/GameCardGrid.vue
+++ src/components/GameCardGrid.vue
@@ -57,7 +57,7 @@ export default {
 
 .games__grid {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(0, 200px));
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
     grid-gap: 20px;
 }
 
