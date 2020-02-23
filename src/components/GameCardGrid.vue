fdaf7e252f47a37855ba479bb64332c8033e20b1
Dennis Burger <dennis@dutchwebworks.nl>
2020-02-23 11:31:17 +0100

    Added filter by platform and sorting

--- src/components/GameCardGrid.vue
+++ src/components/GameCardGrid.vue
@@ -1,16 +1,48 @@
 <template>
-    <div
-        class="games__grid">
-        <GameCardItem
-            v-for="game in games"
-            :key="game.id"
-            :game="game"
-            :platformType="platformType"
-            :genreType="genreType"
-            :mediaType="mediaType"
-            @editGame="editGame"
-            @deleteGame="deleteGame">
-        </GameCardItem>
+    <div>
+        <h3 class="sub-heading">Filter by platform:</h3>
+
+        <ul class="filter">
+            <li 
+                v-for="(platform, index) in platforms"
+                :key="index"
+                class="filter__item paragraph">
+                <label class="filter__label">
+                    <input type="checkbox" v-model="filteredByPlatform" :value="platform">
+                    {{ platformType[platform].title }}
+                </label>
+            </li>
+        </ul>
+
+        <h3 class="sub-heading">Sort by:</h3>
+
+        <div>
+            <select
+                v-model="sortedBy">
+                <option                    
+                    v-for="(sort, index) in sortBy"
+                    :key="index"
+                    :value="sort.value">
+                    {{ sort.name }}
+                </option>
+            </select>
+        </div>
+
+        <h2 class="sub-heading">{{ showingTotal }}</h2>
+
+        <div
+            class="games__grid">
+            <GameCardItem
+                v-for="game in filteredGames"
+                :key="game.id"
+                :game="game"
+                :platformType="platformType"
+                :genreType="genreType"
+                :mediaType="mediaType"
+                @editGame="editGame"
+                @deleteGame="deleteGame">
+            </GameCardItem>
+        </div>
     </div>
 </template>
 
@@ -39,6 +71,33 @@ export default {
             required: true
         }
     },
+    data() {
+        return {
+            platforms: [],
+            sortBy: [
+                {
+                    name: "Title",
+                    value: "title"
+                },
+                {
+                    name: "Publication year",
+                    value: "publicationYear"
+                },
+                {
+                    name: "Purchase year",
+                    value: "purchaseYear"
+                }
+            ],
+            filteredByPlatform: [],
+            sortedBy: ""
+        }
+    },
+    created() {
+        this.getPlatformIds();
+    },
+    mounted() {
+        this.$nextTick(function() { this.sortedBy = this.sortBy[0].value });        
+    },
     methods: {
         editGame(game) {
             this.$emit("editGame", game);
@@ -46,7 +105,25 @@ export default {
         deleteGame(game) {
             this.$emit("deleteGame", game);
         },
-    }
+        getPlatformIds() {
+            this.platforms = _.uniq(_.map(this.games, 'platformId'));
+        },
+        gamePassesPlatformFilter(game) {
+            if(!this.filteredByPlatform.length) {
+                return true;
+            } else {
+                return this.filteredByPlatform.find(platformId => game.platformId === platformId);
+            }
+        }
+    },
+    computed: {
+        showingTotal() {
+            return "Showing: " +  this.filteredGames.length + " result" + (this.filteredGames.length > 0 ? "s" : "");
+        },
+        filteredGames() {
+            return _.sortBy(this.games.filter(this.gamePassesPlatformFilter), this.sortedBy);
+        }        
+    },
 }
 </script>
 
@@ -65,7 +142,19 @@ export default {
 // Element
 // ---------------------------------------------
 
+.filter {
+    display: flex;
+    margin: 0;
+    padding: 0;
+}
 
+.filter__item {
+    display: flex;
+    align-self: center;
+    list-style: none;
+    margin-right: 10px;
+    font-size: 22px;
+}
 
 // ---------------------------------------------
 // Modifier
